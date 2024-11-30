c8b2e75e219cd46937422cbdbf906361a27d993e
nadermnejli <nadermnejli77@gmail.com>
2024-11-30 14:20:39 +0100

    firstCommit Here

---
+++ Core/AppContext/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Core.AppContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Define DbSet for User entity
+        public DbSet<User> Users { get; set; } // Corrected property name and added getter/setter
+
+        // Constructor (optional if using dependency injection)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Optional: Configure the database connection here if not using dependency injection
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          
+        }
+    }
+}
---
+++ Core/Migrations/20241130130537_firstmigration.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    LastName = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Age = table.Column<int>(type: "int", nullable: true),
+                    Email = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
